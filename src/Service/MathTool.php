ffc15456e1d92f7903e6312f4cc52de0ca91d3ea
KieronGrace <KieronRoberson@gmail.com>
2021-06-22 16:47:26 -0500

    played with math tool functionality

---
+++ src/Service/MathTool.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+/**
+ *
+ */
+class MathTool
+{
+
+  function OddOrEven($num) {
+
+    Print_r($_POST);
+  //   if(is_numerical($num)){
+  //     if($num % 2 == 0){
+  //       Print "The number {$num} is even.";
+  //     } else {
+  //       Print "The number {$num} is odd.";
+  //     }
+  //   } else {
+  //     Print "Error: Please enter a number.";
+  //   };
+  // }
+
+}
