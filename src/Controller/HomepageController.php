25ac79cb7fa05565c45662bdca70a1c8f4b6679e
KieronGrace <KieronRoberson@gmail.com>
2021-06-23 17:05:54 -0500

    added odd or even widget

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -13,10 +17,39 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
      */
-    public function homepage()
+
+    public function homepage(Request $request)
     {
-      return $this->render('homepage.html.twig');
+      //odd or even
+      $oddEven = $this->createFormBuilder()
+        ->add('num', NumberType::class,
+            ['label' => 'Enter a number to see if it\'s odd or even!'])
+        ->getForm();
+
+      $oddEven->handleRequest($request);
+
+      if($oddEven->isSubmitted() && $oddEven->isValid()) {
+        $data = $oddEven->getData();
+
+        $num = $data['num'];
+
+        if($num % 2 == 0) {
+          $m = "The number {$num} is even.";
+        } else {
+          $m = "The number {$num} is odd.";
+        }
+
+        return $this->render('homepage.html.twig', [
+          'oddEven' => $oddEven->createView(),
+          'message' => $m,
+        ]);
+      }
+
+      return $this->render('homepage.html.twig', [
+        'oddEven' => $oddEven->createView(),
+      ]);
     }
+
 }
