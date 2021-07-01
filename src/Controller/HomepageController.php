08a1717bb7f082cf6766d555a2cda2ae2bcbe57a
KieronGrace <KieronRoberson@gmail.com>
2021-07-01 17:36:16 -0500

    re-integrated odd even tool with original page.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,23 +3,51 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-/**
- *
- */
 class HomepageController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/")
      */
 
-    public function homepage()
+    public function homepage(Request $request)
     {
-      return $this->render('homepage.html.twig');
+      //odd or even
+      $oddEven = $this->createFormBuilder()
+        ->add('num', NumberType::class,
+            ['label' => 'Enter a number to see if it\'s odd or even!'])
+        ->add('submit', SubmitType::class)
+        ->getForm();
+
+      $oddEven->handleRequest($request);
+
+      if($oddEven->isSubmitted() && $oddEven->isValid()) {
+        $data = $oddEven->getData();
+
+        $num = $data['num'];
+
+        if($num % 2 == 0) {
+          $m = "The number {$num} is even.";
+        } else {
+          $m = "The number {$num} is odd.";
+        }
+
+        return $this->render('homepage.html.twig', [
+          'oddEven' => $oddEven->createView(),
+          'message' => $m,
+        ]);
+      }
+
+      return $this->render('homepage.html.twig', [
+        'oddEven' => $oddEven->createView(),
+      ]);
     }
 
 }
